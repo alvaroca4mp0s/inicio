3237b6753f5358effc9e038b6af03fdebe15630a
RedLocal <alvaro@redlocal.cl>
2018-08-25 13:32:29 -0300

    mail

--- js/main.js
+++ js/main.js
@@ -77,7 +77,7 @@ $(document).ready(function(){
             e.preventDefault(); // prevent default form submit
 
             $.ajax({
-                url: 'mail.php', // form action url
+                url: 'https://formspree.io/alvaro@redlocal.cl', // form action url
                 type: 'POST', // form submit method get/post
                 dataType: 'html', // request type html/json/xml
                 data: form.serialize(), // serialize form data
